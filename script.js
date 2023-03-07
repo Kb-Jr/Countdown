746cc9328c440e153aa887ea273db2f01c8b4480
Kb-Jr <abdulkabiruj@gmail.com>
2023-03-07 18:17:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ dateEl.setAttribute('min', today);
 
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
 
 function updateCountdown(e){
     e.preventDefault();
     let countdownTitle = e.srcElement[0].value;
     let countdownDate = e.srcElement[1].value;
+
+    countdownValue = new Date(countdownDate).getTime();
+    console.log('countdown value:', countdownValue)
 
 }
 
